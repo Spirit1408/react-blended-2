84dcead168ba41970d35b247699a8c5eb98b6eb0
Yarik2019 <yroslavterno@gmail.com>
2024-09-15 15:46:59 +0300

    add todos list and item

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,3 +1,12 @@
-export const TodoList = () => {
-  return <h3>TodoList</h3>;
+import { Grid, GridItem, TodoListItem } from 'components';
+export const TodoList = ({ todos, deleteTodo }) => {
+  return (
+    <Grid>
+      {todos.map((todo, index) => (
+        <GridItem key={todo.id}>
+          <TodoListItem todo={todo} count={index} deleteTodo={deleteTodo} />
+        </GridItem>
+      ))}
+    </Grid>
+  );
 };
