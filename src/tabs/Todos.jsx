95e92f4d38f08b4aa6010fe315f5c9631df7a903
Oleh Shalashov <spirit14081992@gmail.com>
2024-09-15 09:06:28 -0300

    First task

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,17 @@
-import { Text } from 'components';
+import { Text, Form } from "components";
+import { nanoid } from "nanoid";
+import { useState } from "react";
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+	const [todos, setTodos] = useState([]);
+	// return <Text textAlign="center">There are no any todos ...</Text>;
+	const addTask = (text) => {
+		const todo = {
+			text,
+			id: nanoid(),
+		};
+		setTodos((prev) => [...prev, todo]);
+	};
+  console.log(todos)
+	return <Form onSubmit={addTask} />;
 };
